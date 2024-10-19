ca0d8528d0915b24e8bb215e4a2566feb1dfed9e
Shrey Vashisth <143152184+TheDarkWorldProgrammer-0003@users.noreply.github.com>
2024-10-19 20:35:29 +0530

    .

---
+++ src/Constant.java
@@ -0,0 +1,15 @@
+public class Constant {
+    public static void main(String agrs[]){
+        int[] arr = new int[]{1,2,3,4,5};
+        arr [0]= arr[0]+10;  // Time C.->O(1)
+        int num = arr[arr.length-1]; //S.C - int (4 bytes) -> O(1)
+        System.out.println(arr[0]); // Time.C -> O(1)
+
+        // O(1) +O(1)=O()-> O(1) // Constant Time Complexity
+        // O(4 bytes) = O(1) // Constant space Complexity
+
+
+
+
+    }
+}
